f6d0c26cee8b218db60d4158e682cb432b422e4b
Angela Naseri <104205840+angnas@users.noreply.github.com>
2024-08-25 19:34:39 +0200

    Parallel routes

---
+++ app/complex-dashboard/layout.tsx
@@ -0,0 +1,24 @@
+export default function RootLayout({
+  children,
+  users,
+  revenue,
+  notifications,
+}: {
+  children: React.ReactNode;
+  users: React.ReactNode;
+  revenue: React.ReactNode;
+  notifications: React.ReactNode;
+}) {
+  return (
+    <>
+      <div>{children}</div>
+      <div style={{ display: "flex" }}>
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          <div>{users}</div>
+          <div>{revenue}</div>
+          <div style={{ display: "flex", flex: 1 }}>{notifications}</div>
+        </div>
+      </div>
+    </>
+  );
+}
