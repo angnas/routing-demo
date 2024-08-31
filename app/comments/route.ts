ad514866590321d7901b368b7f12d3978de62810
Angela Naseri <104205840+angnas@users.noreply.github.com>
2024-08-31 11:05:26 +0200

    handling POST request

--- app/comments/route.ts
+++ app/comments/route.ts
@@ -1,6 +1,26 @@
+import { request } from "http";
 import { comments } from "./data";
+import { json } from "stream/consumers";
 
 export async function GET() {
     return Response.json(comments)
+    
+}
+
+export async function POST(request : Request) {
+    const comment = await request.json()
+const newComment = {
+    id : comments.length + 1,
+    text: comment.text,
+}
+comments.push(newComment)
+
+
+    return new Response(JSON.stringify(newComment), {
+        headers :  {
+            "Content-Type" : "application/json"
+        }, 
+        status : 201,
+    })
     
 }
